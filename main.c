0509c15362c0ba6fc9038f9123b4a1e68710842c
GustavoFetsch <gustavo.fetsch@escola.pr.gov.br>
2025-09-19 08:34:14 -0300

    vetor

--- main.c
+++ main.c
@@ -1,23 +1,34 @@
-#include <stdio.h>
+#include  <stdio.h>
 
-void main () {
-    int numeros[5];
-    
-    for(int i =0;  i < 5; i++){
-        printf("digite o valor de %d: ", i );
-        scanf("%d",  & numeros[i]);
+
+void main() {
+	int vetor[3];
+
+    for(int i=0; i <3; i++){
+
+    printf(" Digite o valor de %d\n", i);
+    scanf("%d", &vetor [i]);
     }
-    
-    printf("exibindo valores d forma crescente: \n");
-    
-    for (int i=0; i  < 5; i++){
-        printf("%d\n", numeros[i]);
-    }
-    
-    printf("\n exibindovalores de forma decrescente\n");
-    
-    for (int i=4; i >=0 ; i--){
-        printf("%d\n", numeros[i]);
-    }
-    
+    	printf("digite o valor de 1:");
+    	scanf("%d\n", &vetor [0]);
+    	printf("digite o valor de 2:");
+    	scanf("%d\n", &vetor [1]);
+    	printf("digite o valor de 3:");
+    	scanf("%d\n", &vetor [2]);
+
+	vetor[0] =1;
+	vetor[1] =2;
+	vetor[2] =3;
+
+
+    	printf ("%d\n", vetor [0]);
+    	printf ("%d\n", vetor [1]);
+    	printf ("%d\n", vetor [2]);
+    	
+    	printf("exibe o valores:");
+
+	for ( int i=0; i <= 2; i++) {
+		printf(" O valor de %d = %d\n", i+1 , vetor[i] );
+	}
+
 }
